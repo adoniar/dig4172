d0642a7dbf6b5c8c34b202b58388e303df487f1a
adoniar <ad522255@ucf.edu>
2025-01-30 07:34:04 -0500

    made changes to code- Adonia

--- js/script.js
+++ js/script.js
@@ -1,9 +1,4 @@
 // script.js 0078 group 02 Adonia Richards 1/23/2025
-document.addEventListener('DOMContentLoaded', () => {
-    const menuToggle = document.querySelector('.menu-toggle');
-    const navMenu = document.querySelector('.nav-menu');
-  
-    menuToggle.addEventListener('click', () => {
-      navMenu.classList.toggle('open');
-    });
-  });
+function navigateTo(page) {
+  window.location.href = page;
+}
